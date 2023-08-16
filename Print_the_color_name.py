fc30bbeff6f43a809d583abe0d89c902f609914d
22A91A04L6 <119720625+22A91A04L6@users.noreply.github.com>
2023-08-16 23:10:28 +0530

    a=input() if(a=='V'):     print("Violet") elif(a=='I'):     print("Indigo") elif(a=='B'):     print("Blue") elif(a=='G'):     print("Green") elif(a=='Y'):     print("Yellow") elif(a=='R'):     print("Red") else:     print("-1")

---
+++ Print_the_color_name.py
@@ -0,0 +1,15 @@
+a=input()
+if(a=='V'):
+    print("Violet")
+elif(a=='I'):
+    print("Indigo")
+elif(a=='B'):
+    print("Blue")
+elif(a=='G'):
+    print("Green")
+elif(a=='Y'):
+    print("Yellow")
+elif(a=='R'):
+    print("Red")
+else:
+    print("-1")
